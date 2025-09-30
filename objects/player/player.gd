f4af24b46ca9c70ae5ec274989b8b9c43251e974
Matheus Picolli Ishibashi <matheus.ishibashi@gmail.com>
2025-09-30 11:03:17 -0300

    Player Animation Sprites

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -1,19 +1,31 @@
 extends CharacterBody2D
 
-const GRAVITY = 300.0
+const GRAVITY: float = 1000.0
 
-var speed = 300.0
+var speed: float = 300.0
+var jump_force: float = -350.0
 
-var jump_force = -450.0
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
 
-var direction_x
-var direction_y
+var direction_x: float = 0.0
+var direction_y: float = 0.0
 
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	
 	direction_x = Input.get_axis("ui_left", "ui_right")
 	velocity.x = speed * direction_x
+	
+	if direction_x and is_on_floor():
+		sprite.play("run")
+	else:
+		sprite.play("idle")
+	
+	if direction_x > 0:
+		sprite.flip_h = false
+	elif direction_x < 0:
+		sprite.flip_h = true
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_force
